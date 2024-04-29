83839913d9e8890d8823331d5291ad0ea144024c
chuff341k <yejungo44@gmail.com>
2024-04-29 20:03:35 +0900

    Monster cs 추가

---
+++ SpartaDeonjeonBattle/Monster.cs
@@ -0,0 +1,37 @@
+﻿namespace SpartaDeonjeonBattle
+{
+    public enum MonsterType
+    {
+        Nomal, Boss
+    }
+
+    internal class Monster
+    {
+        public string Name { get; }
+        public int Atk { get; }
+        public int Hp { get; }
+
+        private MonsterType Type;
+        public bool IsLife { get; private set; }
+        public bool IsDead { get; private set; }
+
+        public Monster(string name, MonsterType type, int atk, int hp, bool isLife = false, bool isDead = false)
+        {
+            Name = name;
+            Type = type;
+            Atk = atk;
+            Hp = hp;
+            IsLife = isLife;
+            IsDead = isDead;
+        }
+        internal void ToggleMonsterStatus()
+        {
+            IsLife = !IsLife;
+        }
+
+        internal void Battle()
+        {
+            IsLife = true;
+        }
+    }
+}
