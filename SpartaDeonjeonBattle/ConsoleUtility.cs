7c60bf0a0f213c2f27ccf5a5e112897a595f0d6c
peterbae9327 <peterbae9327@gmail.com>
2024-04-29 14:53:33 +0900

    build basic functions

---
+++ SpartaDeonjeonBattle/ConsoleUtility.cs
@@ -0,0 +1,54 @@
+﻿namespace SpartaDeonjeonBattle
+{
+    internal class ConsoleUtility
+    {
+        public static int MenuChoice(int min, int max)
+        {
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요");
+            Console.Write(">>");
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int choice) && choice >= min && choice <= max)
+                {
+                    return choice;
+                }
+                Console.WriteLine("잘못된 입력입니다. 다시 선택해주세요");
+                HighlightTxt(">>", ConsoleColor.Yellow);
+            }
+        }
+        public static int ObjectChoice(int min, int max)
+        {
+            Console.WriteLine();
+            Console.WriteLine("대상을 선택해주세요");
+            Console.Write(">>");
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int choice) && choice >= min && choice <= max)
+                {
+                    return choice;
+                }
+                Console.WriteLine("잘못된 입력입니다. 다시 선택해주세요");
+                HighlightTxt(">>", ConsoleColor.Yellow);
+            }
+        }
+        public static void Getout(string action)
+        {
+            Console.WriteLine();
+            Console.WriteLine("0. " + action);
+        }
+        public static void ShowTitle(string title)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(title);
+            Console.ResetColor();
+        }
+        public static void HighlightTxt(string highlighted, ConsoleColor choosecolor)
+        {
+            Console.ForegroundColor = choosecolor;
+            Console.Write(highlighted);
+            Console.ResetColor();
+        }
+
+    }
+}
--- SpartaDeonjeonBattle/Program.cs
+++ SpartaDeonjeonBattle/Program.cs
@@ -1,10 +1,56 @@
-﻿namespace SpartaDeonjeonBattle
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+namespace SpartaDeonjeonBattle
 {
+    public class GameManager
+    {
+        private Player player;
+
+        public GameManager()
+        {
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            player = new Player("CHAD", "전사", 1, 10, 5, 100, 1500);
+        }
+
+        public void StartGame()
+        {
+            Console.Clear();
+            MainMenu();
+        }
+
+        private void MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("이제 전투를 시작할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("1. 상태 보기");
+            Console.WriteLine("2. 전투 시작");
+            int choice = ConsoleUtility.MenuChoice(1, 2);
+            switch (choice)
+            {
+                case 1:
+                    //상태보기
+                    break;
+                case 2:
+                    //전투시작
+                    break;
+            }
+            
+
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GameManager gamemanager = new GameManager();
+            gamemanager.StartGame();
         }
     }
 }
