66d4b31555872d85c39eec11a31cca742add74f2
chuff341k <yejungo44@gmail.com>
2024-05-01 16:42:25 +0900

    포션 cs 추가

---
+++ SpartaDeonjeonBattle/Potion.cs
@@ -0,0 +1,25 @@
+﻿namespace SpartaDeonjeonBattle
+{
+    public enum PotionType
+    {
+        HEAL
+    }
+
+    internal class Potion
+    {
+        public string Name { get; }
+        public string Desc { get; }
+
+        private PotionType Type;
+
+        public int Hp { get; }
+
+        public Potion(string name, string desc, PotionType type, int hp)
+        {
+            Name = name;
+            Desc = desc;
+            Type = type;
+            Hp = hp;
+        }
+    }
+}
