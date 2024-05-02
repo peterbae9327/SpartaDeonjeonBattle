c37b082c365af20ef5e4e2e161800cbcc1345d72
chuff341k <yejungo44@gmail.com>
2024-05-02 15:45:22 +0900

    Inventory cs 수정

--- SpartaDeonjeonBattle/Inventory.cs
+++ SpartaDeonjeonBattle/Inventory.cs
@@ -2,6 +2,44 @@
 {
     internal class Inventory
     {
+        private Player player;
+        private GameManager manager;
+
+        public Inventory(Player player, GameManager manager)
+        {
+            this.player = player;
+            this.manager = manager;
+        }
+
+        public void InventoryMenu()
+        {
+            Console.Clear();
+
+            ConsoleUtility.ShowTitle("■ 인벤토리 ■");
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+            Console.WriteLine("");
+            Console.WriteLine("[아이템 목록]");
+
+            for (int i = 0; i < inventory.Count; i++)
+            {
+                inventory[i].PrintItemStatDescription();
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine("1. 장착관리");
+            Console.WriteLine("");
+
+            switch (ConsoleUtility.PromptMenuChoice(0, 1))
+            {
+                case 0:
+                    MainMenu();
+                    break;
+                case 1:
+                    EquipMenu();
+                    break;
+            }
+        }
 
     }
 }
