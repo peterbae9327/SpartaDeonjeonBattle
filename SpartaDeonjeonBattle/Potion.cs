49f8f6a42fd442bdb5ef155dd176888d12dbb137
chuff341k <yejungo44@gmail.com>
2024-05-03 16:00:02 +0900

    Update Potion.cs

--- SpartaDeonjeonBattle/Potion.cs
+++ SpartaDeonjeonBattle/Potion.cs
@@ -10,7 +10,6 @@ namespace SpartaDeonjeonBattle
         public int Hp { get; }
         public int Quantity { get; set; }
 
-        // 왼쪽부터 포션 이름, 설명, 타입, 타입 별 효과, 현재 수량
         public Potion(string name, string desc, int hp, int quantity) 
         {
             Name = name;
